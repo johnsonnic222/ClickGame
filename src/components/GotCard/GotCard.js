29f537066ed5e36507bae83f9e5207a6ee7c9c76
Nicole Johnson <johsnonbadgersnic222@gmail.com>
2018-07-16 20:23:59 -0400

    game

---
+++ src/components/GotCard/GotCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./GotCard.css";
+
+const GotCard = props => (
+    <div 
+        className="card"
+        value = {props.id}
+        onClick={() => props.handleClick(props.id)}
+    >
+
+    <div 
+        className = "img-container">
+        <img alt={props.name} src={props.image} />
+    </div>
+    </div>
+
+);
+
+export default GotCard;
+
